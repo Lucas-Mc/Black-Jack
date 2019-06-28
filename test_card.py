570972e70eeb11fbfd910c812f09b4ad91184396
Lucas-Mc <lumccul3@gmail.com>
2019-06-28 14:39:57 -0600

    Finished card unit test

--- test_card.py
+++ test_card.py
@@ -16,5 +16,10 @@ class TestCard(unittest.TestCase):
   def test_value(self):
     self.assertEqual(Card.get_value(self.card1), TestCard._value)
 
+  def test__str__(self):
+    t_string = str(TestCard._value) + ' of ' + TestCard._suit
+    self.assertEqual(str(self.card1), t_string)
+
+
 if __name__ == '__main__':
   unittest.main()
