1af44b4703d2983d03f4b8c41ffecb63c0f08b66
Lucas-Mc <lumccul3@gmail.com>
2019-06-28 14:19:30 -0600

    Added dealer unit test

--- test_card.py
+++ test_card.py
@@ -3,15 +3,18 @@ from card import Card
 
 class TestCard(unittest.TestCase):
 
+  _suit = 'heart'
+  _value = 10
+
   @classmethod
   def setUpClass(cls):
-    cls.card1 = Card('heart',10)
+    cls.card1 = Card(TestCard._suit,TestCard._value)
 
   def test_suit(self):
-    self.assertEqual(Card.get_suit(self.card1), 'heart')
+    self.assertEqual(Card.get_suit(self.card1), TestCard._suit)
 
   def test_value(self):
-    self.assertEqual(Card.get_value(self.card1), 10)
+    self.assertEqual(Card.get_value(self.card1), TestCard._value)
 
 if __name__ == '__main__':
   unittest.main()
---
+++ test_dealer.py
@@ -0,0 +1,13 @@
+import unittest
+from dealer import Dealer
+
+class TestCardlist(unittest.TestCase):
+
+  def test_deal(self):
+    dealer = Dealer()
+    self.assertEqual(len(dealer.cards),52)
+    dealer.deal()
+    self.assertEqual(len(dealer.cards),51)
+
+if __name__ == '__main__':
+  unittest.main()
